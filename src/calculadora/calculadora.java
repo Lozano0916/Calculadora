762e5027dbe9ef1ca9b18aa1a97a9d0f5d3b707a
Jairo Eduardo <jairoedo2@gmail.com>
2023-01-25 22:52:40 -0500

    calculadora

--- src/calculadora/calculadora.java
+++ src/calculadora/calculadora.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package calculadora;
 
 import java.awt.Image;
@@ -9,7 +6,7 @@ import java.awt.Toolkit;
 
 /**
  *
- * @author Usuario
+ * @lozano0916
  */
 public class calculadora extends javax.swing.JFrame {
 
